d1516f875c8f2c66fd2c6775ad9efaf7f34e1c93
unknown <duylong0601@gmail.com>
2025-03-26 14:04:34 +0700

    update_2603

--- resources/views/user/sections/dashboard/index.blade.php
+++ resources/views/user/sections/dashboard/index.blade.php
@@ -1,9 +1,21 @@
 @extends('user.layouts.master')
 
-@section('title', 'Dashboard')
+@section('title', 'Bảng tin')
 
 @section('contents')
 
-
+<div class="row justify-content-center">
+    <div class="col-6">
+        <div class="card text-center p-3">
+            @php($user = auth()->guard('web')->user())
+            <div class="row justify-content-center mb-3">
+                <div class="col-auto">
+                    <img src="{{ asset($user->profile_pic) }}" alt="" class="img-fluid rounded-circle border" height="100" width="100">
+                </div>
+            </div>
+            <div class="span">Chào <strong class="font-italic">{{ $user->fullname }}</strong></div>
+        </div>
+    </div>
+</div>
 
 @endsection
